fa0a640a9fa2537dac27acf7bf59bb52e250b53c
Robert <hello@rooosh.com>
2021-05-01 20:07:52 +0200

    add babel.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
-module.exports = {
-    mode: "development",
+let mode = "development";
 
-    devtool: false,
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+}
+
+module.exports = {
+    mode: mode,
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+
+    devtool: "source-map",
     devServer: {
         contentBase: "./dist",
     },
