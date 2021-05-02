388ddd76eda706644c8b79e05dd52cce57fd3838
Robert <hello@rooosh.com>
2021-05-02 13:35:18 +0200

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
 
     module: {
         rules: [
             {
-                test: /\.s?css$/i,
+                test: /\.(s[ac]|c)css$/i,
                 use: [MiniCssExtractPlugin.loader,
                     "css-loader",
                     "postcss-loader",
